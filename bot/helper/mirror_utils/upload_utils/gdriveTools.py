0b56016fae0d13d5a9f5d12a9a105ccae209dc8e
JiC54 <jic54studio@gmail.com>
2022-09-26 12:16:04 +0200

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -783,34 +783,34 @@ class GoogleDriveHelper:
             if mime_type == self.__G_DRIVE_DIR_MIME_TYPE:
                 self.__gDrive_directory(meta)
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{name}</code>'
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├♻ Type: </b>Folder'
-                    msg += f'\n<b>├🗃️ SubFolders: </b>{self.__total_folders}'
+                    msg += f'<b>🗂️ Name: </b><code>{name}</code>'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>♻ Type: </b>Folder'
+                    msg += f'\n<b>🗃️ SubFolders: </b>{self.__total_folders}'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{name}</code>'
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├ Type: </b>Folder'
-                    msg += f'\n<b>├ SubFolders: </b>{self.__total_folders}'
+                    msg += f'<b>Name: </b><code>{name}</code>'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>Type: </b>Folder'
+                    msg += f'\n<b>SubFolders: </b>{self.__total_folders}'
             else:
                 if EMOJI_THEME is True:
-                    msg += f'<b>╭🗂️ Name: </b><code>{name}</code>'
+                    msg += f'<b>🗂️ Name: </b><code>{name}</code>'
                 else:
-                    msg += f'<b>╭ Name: </b><code>{name}</code>'
+                    msg += f'<b>Name: </b><code>{name}</code>'
                 if mime_type is None:
                     mime_type = 'File'
                 self.__total_files += 1
                 self.__gDrive_file(meta)
                 if EMOJI_THEME is True:
-                    msg += f'\n<b>├📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├♻ Type: </b>{mime_type}'
+                    msg += f'\n<b>📦 Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>♻ Type: </b>{mime_type}'
                 else:
-                    msg += f'\n<b>├ Size: </b>{get_readable_file_size(self.__total_bytes)}'
-                    msg += f'\n<b>├ Type: </b>{mime_type}'
+                    msg += f'\n<b>Size: </b>{get_readable_file_size(self.__total_bytes)}'
+                    msg += f'\n<b>Type: </b>{mime_type}'
             if EMOJI_THEME is True:
-                msg += f'\n<b>├🗂️ Files: </b>{self.__total_files}'
+                msg += f'\n<b>🗂️ Files: </b>{self.__total_files}'
             else:
-                msg += f'\n<b>├ Files: </b>{self.__total_files}'
+                msg += f'\n<b>Files: </b>{self.__total_files}'
         except Exception as err:
             if isinstance(err, RetryError):
                 LOGGER.info(f"Total Attempts: {err.last_attempt.attempt_number}")
